cbf46e641555f0d32ae3839bcf8f4c876104b28e
allords <70876003+allords@users.noreply.github.com>
2020-11-30 22:48:36 -0700

    Update ScatterPlot.java

--- Final Project/src/ScatterPlot.java
+++ Final Project/src/ScatterPlot.java
@@ -13,14 +13,6 @@ public class ScatterPlot extends JFrame {
 
     private Information info;
 
-    public void setRoster(Information info) {
-        this.info = info;
-    }
-    public Information getRoster() {
-        return info;
-    }
-
-
     public ScatterPlot(String title, Information info) {
         super(title);
         this.info = info;
@@ -32,11 +24,9 @@ public class ScatterPlot extends JFrame {
                 "Attendance Report",
                 "Percentage of Lecture Attended (min attended / total min of lecture)", "Students", dataset);
 
-
         //Changes background color
         XYPlot plot = (XYPlot)chart.getPlot();
         plot.setBackgroundPaint(new Color(255,228,196));
-
 
         // Create Panel
         ChartPanel panel = new ChartPanel(chart);
@@ -46,28 +36,30 @@ public class ScatterPlot extends JFrame {
     private XYDataset createDataset() {
         XYSeriesCollection dataset = new XYSeriesCollection();
 
+        // for all date columns (after roster info)
         for ( int dateColIndex = 0; dateColIndex < info.getDateColCount(); dateColIndex++) {
-            XYSeries series = new XYSeries(info.getHeaderRow()[ 6 + dateColIndex]);
-            int percentage = 0;
-            int roundedPercentage = 0;
+            XYSeries series = new XYSeries(info.getHeaderRow()[6 + dateColIndex]); // gets date from header string array
+            int percentage;
+            int roundedPercentage;
             int[] tallyArray = new int[11]; // each index represents a percentage tally. 0 = 0%, 1 = 10% ... 10 = 100%
-            int tallyIndex = 0;
+            int tallyIndex;
 
             for (int rowIndex = 0; rowIndex < info.getRosterRowCount(); rowIndex++) {
 
-                percentage = (100*(info.getAttendancePoint(rowIndex, dateColIndex)))/75;
-                roundedPercentage = ((percentage+5)/10)*10;
-                if (roundedPercentage > 100) {
+                percentage = (100*(info.getAttendancePoint(rowIndex, dateColIndex)))/75; // based off 75 minute class session
+                roundedPercentage = ((percentage+5)/10)*10; // rounds to nearest 10's place
+                if (roundedPercentage > 100) { // student was there for longer than the session then set to 100% index (10)
                     tallyIndex = 10;
                 } else {
                     tallyIndex = roundedPercentage / 10;
                 }
 
-                tallyArray[tallyIndex]++;
+                tallyArray[tallyIndex]++; // increment the tally at the representative percentage index
             }
 
+            // for all percentage possibilities
             for (int i = 0; i < 11; i++) {
-                series.add(i*10, tallyArray[i]);
+                series.add(i*10, tallyArray[i]); // add tally value at its percentage value
             }
             dataset.addSeries(series);
         }
